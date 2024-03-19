461f6980c30128aa5f052beee13db82f3be13c8b
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2024-03-19 20:25:53 +0530

    First Commit

---
+++ src/main/java/com/mugenminds/mugenminds/MugenMindsApplication.java
@@ -0,0 +1,13 @@
+package com.mugenminds.mugenminds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugenMindsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugenMindsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mugenminds/mugenminds/MugenMindsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugenminds.mugenminds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugenMindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
